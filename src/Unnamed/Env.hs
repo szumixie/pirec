744e51cfada813daa9106bd448122229cb95a226
Szumi Xie <szumixie@gmail.com>
2021-03-16 04:10:44 +0100

    Use RList for env

--- src/Unnamed/Env.hs
+++ src/Unnamed/Env.hs
@@ -2,24 +2,29 @@ module Unnamed.Env (Env, empty, extend, index, level) where
 
 import Relude hiding (empty)
 
+import Data.Functor.Reverse (Reverse (..))
+
 import Optics
 
+import Unnamed.Data.RList (RList)
+import Unnamed.Data.RList qualified as R
 import Unnamed.Var.Level (Level (..))
 
-newtype Env a = Env (Seq a)
-  deriving newtype (Show, Foldable)
+newtype Env a = Env (RList a)
+  deriving newtype (Show)
+  deriving (Foldable) via Reverse RList
 
 instance FoldableWithIndex Level Env where
-  ifoldMap f (Env xs) = ifoldMap (f . coerce) xs
+  ifoldMap f (Env xs) = xs & ifoldMap \i -> f $ Level (length xs - i - 1)
 
 empty :: Env a
-empty = Env mempty
+empty = Env R.empty
 
-extend :: a -> Env a -> Env a
-extend x (Env xs) = Env $ xs |> x
+extend :: forall a. a -> Env a -> Env a
+extend = coerce (R.cons @a)
 
 index :: Level -> Env a -> Maybe a
-index (Level i) (Env xs) = xs ^? ix i
+index (Level i) (Env xs) = xs ^? ix (length xs - i - 1)
 
-level :: Env a -> Level
-level (Env xs) = Level $ length xs
+level :: forall a. Env a -> Level
+level = coerce (length @RList @a)
