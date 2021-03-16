f40430cc1597b816f276b9355b8210e4d7b359fe
Szumi Xie <szumixie@gmail.com>
2021-03-16 03:34:37 +0100

    Remove withPos combinator

--- src/Unnamed/WithPos.hs
+++ src/Unnamed/WithPos.hs
@@ -1,4 +1,4 @@
-module Unnamed.WithPos (WithPos (..), withPos) where
+module Unnamed.WithPos (WithPos (..)) where
 
 import Relude
 
@@ -6,6 +6,3 @@ import Text.Megaparsec (SourcePos)
 
 data WithPos a = WithPos SourcePos a
   deriving stock (Show)
-
-withPos :: (SourcePos -> a -> r) -> WithPos a -> r
-withPos f (WithPos pos x) = f pos x
